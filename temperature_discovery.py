5d20db24a7cee28c6a857838aebf51fe56105dbe
gnom <ggskas@gmail.com>
2017-01-08 17:41:14 +0300

    1.1

--- temperature_discovery.py
+++ temperature_discovery.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from subprocess import Popen, PIPE
 import re
+import mod_json
 x = 'sensors'
 y = str(Popen(x, shell=True, stdin=PIPE, stdout=PIPE).stdout.read())
 stdout = re.findall('\w\w\w\w\d+|\w\w\w\w \d+', y)

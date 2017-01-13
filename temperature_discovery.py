bc60645f7c5e33d38e8ceeaf17ac6b1983fcf903
gnom <ggskas@gmail.com>
2017-01-13 08:28:08 +0300

    1.1

--- temperature_discovery.py
+++ temperature_discovery.py
@@ -5,7 +5,6 @@ import re
 import mod_json
 cmd = 'sensors'
 stdout = str(Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE).stdout.read())
-print(stdout)
-stdout = re.findall(r'\\n(.*?)[+]', stdout)
+stdout = re.findall(r'\\n(\w+\d|\w+ \d):.*?\d\d', stdout)
 fname = 'TEMPSENS'
 mod_json.main(stdout, fname)

d63a5fc646c4cfc477337c832be9ff68ebe39057
gnom4ik <jr.pashkin@gmail.com>
2017-01-15 00:07:22 +0300

    1.1

--- ps_discovery.py
+++ ps_discovery.py
@@ -4,8 +4,8 @@
 from subprocess import Popen, PIPE
 import re
 import mod_json
-command = 'cat /etc/passwd'
+command = 'ps -eo command |sed \'s/\[//g;s/\]//g;s/^-//g\' |uniq'
 stdout = str(Popen(command, shell=True, stdin=PIPE, stdout=PIPE).stdout.read()).replace('b\'','\\n')
-stdout = re.findall(r'\\n(.*?):', stdout)
-fname = 'USERNAME'
+stdout = re.findall(r'\\n(.*?)\\n', stdout.strip('\\nCOMMAND\\n'))
+fname = 'PROCESS'
 mod_json.main(stdout, fname)

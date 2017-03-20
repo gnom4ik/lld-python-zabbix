3ca07fdfafb309cce7dbc3d47d23a826aa1e2de1
gnom4ik <jr.pashkin@gmail.com>
2017-03-20 17:39:59 +0300

    Update ps_discovery.py

--- share/zabbix/externalscripts/ps_discovery.py
+++ share/zabbix/externalscripts/ps_discovery.py
@@ -6,10 +6,6 @@ from subprocess import Popen, PIPE
 
 from share.zabbix.externalscripts import mod_json
 
-
-def
-
-
 command = 'ps -eo %cpu,command |sort -g |tail -n10 |sed \'s/^....//g;s/^ //g\' |cut -c1-50'
 stdout = str(Popen(command, shell=True, stdin=PIPE, stdout=PIPE).stdout.read()).replace('b\'','\\n')
 stdout = re.findall(r'\\n(.*?)\\n', stdout.strip('\\nCOMMAND\\n'))

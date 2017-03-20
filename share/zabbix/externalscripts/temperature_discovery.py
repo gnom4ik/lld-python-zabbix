4bd397009cbf9b9962534b272fbf220b9f1e73e6
gnom4ik <jr.pashkin@gmail.com>
2017-03-20 17:44:27 +0300

    Update temperature_discovery.py

--- share/zabbix/externalscripts/temperature_discovery.py
+++ share/zabbix/externalscripts/temperature_discovery.py
@@ -3,7 +3,7 @@
 import re
 from subprocess import Popen, PIPE
 
-from share.zabbix.externalscripts import mod_json
+import mod_json
 
 cmd = 'sensors'
 stdout = str(Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE).stdout.read())
